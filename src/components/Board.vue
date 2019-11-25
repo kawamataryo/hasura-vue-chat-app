8c7f9e4faf80cc187b6a71bdc6d9e93b89807edb
ryo <ba068082@gmail.com>
2019-11-26 05:36:23 +0900

    init apollo

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,13 +1,31 @@
 <template>
-  <h1>Hello</h1>
+  <div>
+    <h1>Hello</h1>
+    <ul>
+      <li v-for="m in messages" :key="m.id">{{ m.content }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import gql from "graphql-tag";
+
 export default {
-  name: "Board"
-}
+  name: "Board",
+  data: () => ({
+    messages: []
+  }),
+  apollo: {
+    messages: gql`
+      query messages {
+        messages {
+          id
+          content
+        }
+      }
+    `
+  }
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
