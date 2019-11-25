1de7dddc25c4ffc09d6ef6a31b5f411ca4fff376
ryo <ba068082@gmail.com>
2019-11-26 05:19:54 +0900

    [from now] 2019/11/26 05:19:54
    diff --git a/.graphqlconfig b/.graphqlconfig
    new file mode 100644
    index 0000000..f6087cf
    --- /dev/null
    +++ b/.graphqlconfig
    @@ -0,0 +1,15 @@
    +{
    +  "name": "Untitled GraphQL Schema",
    +  "schemaPath": "schema.graphql",
    +  "extensions": {
    +    "endpoints": {
    +      "Default GraphQL Endpoint": {
    +        "url": "http://localhost:8000/v1/graphql",
    +        "headers": {
    +          "user-agent": "JS GraphQL"
    +        },
    +        "introspect": false
    +      }
    +    }
    +  }
    +}
    diff --git a/schema.graphql b/schema.graphql
    new file mode 100644
    index 0000000..e69de29
    diff --git a/src/components/board.vue b/src/components/board.vue
    index 4d77536..5e6daba 100644
    --- a/src/components/board.vue
    +++ b/src/components/board.vue
    @@ -1,13 +1,29 @@
     <template>
       <h1>board</h1>
    +  <li v-for="(m, i) in messages" :key="i">{{m.content}}</li>
     </template>
    
    -<script lang="ts">
    +<script>
     import Vue from "vue";
    +import gql from "graphql-tag";
    
    -export default Vue.extend({
    -  name: "board"
    -});
    +export default {
    +  name: "board",
    +  data() {
    +    return {
    +      messages: {}
    +    };
    +  },
    +  apollo: {
    +    messages: gql`
    +      query {
    +        messages {
    +          content
    +        }
    +      }
    +    `
    +  }
    +};
     </script>
    
     <style scoped></style>
    diff --git a/src/vue-apollo.js b/src/vue-apollo.ts
    similarity index 100%
    rename from src/vue-apollo.js
    rename to src/vue-apollo.ts
    diff --git a/tsconfig.json b/tsconfig.json
    index 6b8d407..9746f63 100644
    --- a/tsconfig.json
    +++ b/tsconfig.json
    @@ -12,7 +12,8 @@
         "baseUrl": ".",
         "types": [
           "webpack-env",
    -      "vuetify"
    +      "vuetify",
    +      "vue-cli-plugin-apollo/graphql-client"
         ],
         "paths": {
           "@/*": [

--- src/components/board.vue
+++ src/components/board.vue
@@ -1,13 +1,29 @@
 <template>
   <h1>board</h1>
+  <li v-for="(m, i) in messages" :key="i">{{m.content}}</li>
 </template>
 
-<script lang="ts">
+<script>
 import Vue from "vue";
+import gql from "graphql-tag";
 
-export default Vue.extend({
-  name: "board"
-});
+export default {
+  name: "board",
+  data() {
+    return {
+      messages: {}
+    };
+  },
+  apollo: {
+    messages: gql`
+      query {
+        messages {
+          content
+        }
+      }
+    `
+  }
+};
 </script>
 
 <style scoped></style>
