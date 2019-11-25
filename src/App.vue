e56e25fb32fe245f8436efe81c36c264d4e32cd0
ryo <ba068082@gmail.com>
2019-11-26 05:27:58 +0900

    add vuetify and clean up defaulut file

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <h1>GraphQL chat app</h1>
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <board></board>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Board from "./components/Board";
 
 export default {
-  name: "app",
+  name: "App",
+
   components: {
-    HelloWorld
-  }
+    Board
+  },
+
+  data: () => ({
+    //
+  })
 };
 </script>
-
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Board.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Hello</h1>
+</template>
+
+<script>
+export default {
+  name: "Board"
+}
+</script>
+
+<style scoped>
+
+</style>
