3a05073441f92ec705f97a075540e19ef8fb46f3
ryo <ba068082@gmail.com>
2019-11-27 04:00:46 +0900

    [from now] 2019/11/27 04:00:46
    diff --git a/src/components/Board.vue b/src/components/Board.vue
    index 670ff1f..fb6e09b 100644
    --- a/src/components/Board.vue
    +++ b/src/components/Board.vue
    @@ -1,6 +1,7 @@
     <template>
       <div>
         <h1>Hello</h1>
    +    <message-form @submit="addMessage"></message-form>
         <ul>
           <li v-for="m in messages" :key="m.id">{{ m.content }}</li>
         </ul>
    @@ -9,14 +10,35 @@
    
     <script>
     import gql from "graphql-tag";
    +import MessageForm from "./MessageForm";
    
     export default {
       name: "Board",
    +  components: {
    +    MessageForm
    +  },
       data() {
         return {
           messages: []
         };
       },
    +  methods: {
    +    async addMessage(content) {
    +      const updateResult = await this.$apollo.mutate({
    +        mutation: gql`
    +          mutation($content: String!) {
    +            insert_messages(objects: { content: $content }) {
    +              affected_rows
    +            }
    +          }
    +        `,
    +        variables: {
    +          content
    +        }
    +      });
    +      console.log(updateResult.data.rowsAffected.messages.length)
    +    }
    +  },
       apollo: {
         messages: {
           query: gql`
    diff --git a/src/components/MessageForm.vue b/src/components/MessageForm.vue
    new file mode 100644
    index 0000000..6bbd148
    --- /dev/null
    +++ b/src/components/MessageForm.vue
    @@ -0,0 +1,29 @@
    +<template>
    +  <form>
    +    <v-text-field
    +        v-model="content"
    +        required
    +        label="content"
    +    >
    +    </v-text-field>
    +    <v-btn @click="submit">submit</v-btn>
    +  </form>
    +</template>
    +
    +<script>
    +export default {
    +  name: "MessageForm",
    +  data: () => ({
    +    content: ""
    +  }),
    +  methods: {
    +    submit() {
    +      this.$emit("submit", this.content);
    +    }
    +  }
    +};
    +</script>
    +
    +<style scoped>
    +
    +</style>

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>Hello</h1>
+    <message-form @submit="addMessage"></message-form>
     <ul>
       <li v-for="m in messages" :key="m.id">{{ m.content }}</li>
     </ul>
@@ -9,13 +10,34 @@
 
 <script>
 import gql from "graphql-tag";
+import MessageForm from "./MessageForm";
 
 export default {
   name: "Board",
+  components: {
+    MessageForm
+  },
   data() {
     return {
       messages: []
     };
+  },
+  methods: {
+    async addMessage(content) {
+      const updateResult = await this.$apollo.mutate({
+        mutation: gql`
+          mutation($content: String!) {
+            insert_messages(objects: { content: $content }) {
+              affected_rows
+            }
+          }
+        `,
+        variables: {
+          content
+        }
+      });
+      console.log(updateResult.data.rowsAffected.messages.length)
+    }
   },
   apollo: {
     messages: {
---
+++ src/components/MessageForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <form>
+    <v-text-field
+        v-model="content"
+        required
+        label="content"
+    >
+    </v-text-field>
+    <v-btn @click="submit">submit</v-btn>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "MessageForm",
+  data: () => ({
+    content: ""
+  }),
+  methods: {
+    submit() {
+      this.$emit("submit", this.content);
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
