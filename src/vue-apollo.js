82506c4f92c4a99a282b9c30e57688e25164b821
ryo <ba068082@gmail.com>
2019-12-21 15:39:33 +0900

    [from now] 2019/12/21 15:39:33
    diff --git a/src/vue-apollo.js b/src/vue-apollo.js
    index cb8e88d..12d65e9 100644
    --- a/src/vue-apollo.js
    +++ b/src/vue-apollo.js
    @@ -2,7 +2,6 @@ import Vue from "vue";
     import { ApolloClient } from "apollo-client";
     import { HttpLink } from "apollo-link-http";
     import { InMemoryCache } from "apollo-cache-inmemory";
    -// New Imports
     import { split } from "apollo-link";
     import { WebSocketLink } from "apollo-link-ws";
     import { getMainDefinition } from "apollo-utilities";
    @@ -10,11 +9,9 @@ import { getMainDefinition } from "apollo-utilities";
     import VueApollo from "vue-apollo";
    
     const httpLink = new HttpLink({
    -  // You should use an absolute URL here
       uri: "http://localhost:8000/v1/graphql"
     });
    
    -// Create the subscription websocket link
     const wsLink = new WebSocketLink({
       uri: "ws://localhost:8000/v1/graphql",
       options: {
    @@ -22,10 +19,7 @@ const wsLink = new WebSocketLink({
       }
     });
    
    -// using the ability to split links, you can send data to each link
    -// depending on what kind of operation is being sent
     const link = split(
    -  // split based on operation type
       ({ query }) => {
         const definition = getMainDefinition(query);
         return (
    @@ -37,14 +31,12 @@ const link = split(
       httpLink
     );
    
    -// Create the apollo client
     const apolloClient = new ApolloClient({
       link,
       cache: new InMemoryCache(),
       connectToDevTools: true
     });
    
    -// Install the vue plugin like before
     Vue.use(VueApollo);
    
     export const apolloProvider = new VueApollo({

--- src/vue-apollo.js
+++ src/vue-apollo.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import { ApolloClient } from "apollo-client";
 import { HttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
-// New Imports
 import { split } from "apollo-link";
 import { WebSocketLink } from "apollo-link-ws";
 import { getMainDefinition } from "apollo-utilities";
@@ -10,11 +9,9 @@ import { getMainDefinition } from "apollo-utilities";
 import VueApollo from "vue-apollo";
 
 const httpLink = new HttpLink({
-  // You should use an absolute URL here
   uri: "http://localhost:8000/v1/graphql"
 });
 
-// Create the subscription websocket link
 const wsLink = new WebSocketLink({
   uri: "ws://localhost:8000/v1/graphql",
   options: {
@@ -22,10 +19,7 @@ const wsLink = new WebSocketLink({
   }
 });
 
-// using the ability to split links, you can send data to each link
-// depending on what kind of operation is being sent
 const link = split(
-  // split based on operation type
   ({ query }) => {
     const definition = getMainDefinition(query);
     return (
@@ -37,14 +31,12 @@ const link = split(
   httpLink
 );
 
-// Create the apollo client
 const apolloClient = new ApolloClient({
   link,
   cache: new InMemoryCache(),
   connectToDevTools: true
 });
 
-// Install the vue plugin like before
 Vue.use(VueApollo);
 
 export const apolloProvider = new VueApollo({
