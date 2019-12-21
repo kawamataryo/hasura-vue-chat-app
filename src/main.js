131156c0b3a17746f496ac844ee5db0b7f1d9473
ryo <ba068082@gmail.com>
2019-12-21 20:55:21 +0900

    move apollo setting to plugins

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
-import { apolloProvider } from "./vue-apollo";
+import { apolloProvider } from "./plugins/vue-apollo";
 
 Vue.config.productionTip = false;
 
