0eb02dfd3ce9e0cccdf78b0b45dbf67d50d59b90
ryo <ba068082@gmail.com>
2020-01-17 20:50:25 +0900

    [from now] 2020/01/17 20:50:25
    diff --git a/.qawolf/tests/deleteMessage.test.js b/.qawolf/tests/deleteMessage.test.js
    index 177bb9b..69efe19 100644
    --- a/.qawolf/tests/deleteMessage.test.js
    +++ b/.qawolf/tests/deleteMessage.test.js
    @@ -15,15 +15,15 @@ describe('deleteMessage', () => {
         await browser.type(selectors[1], "message to be deleted");
         await browser.click(selectors[2]);
    
    -    const hasMessage = await browser.hasText("message to be deleted");
    -    expect(hasMessage).toEqual(true);
    -
    -    const messagesElement = await browser.find({css: ".messages-wrapper"});
    -    const messagesText = await messagesElement.evaluate(node => node.innerText);
    +    let messagesElement = await browser.find({css: ".messages-wrapper"});
    +    let messagesText = await messagesElement.evaluate(node => node.innerText);
         expect(messagesText).toContain("message to be deleted");
    
         // delete message
         await browser.click(selectors[3]);
    -    expect(hasMessage).toEqual(false);
    +
    +    messagesElement = await browser.find({css: ".messages-wrapper"});
    +    messagesText = await messagesElement.evaluate(node => node.innerText);
    +    expect(messagesText).not.toContain("message to be deleted");
       });
     });
    diff --git a/.qawolf/tests/sendMessage.test.js b/.qawolf/tests/sendMessage.test.js
    index 9fa8553..6eebe5e 100644
    --- a/.qawolf/tests/sendMessage.test.js
    +++ b/.qawolf/tests/sendMessage.test.js
    @@ -14,9 +14,6 @@ describe('sendMessage', () => {
         await browser.type(selectors[1], "sample message");
         await browser.click(selectors[3]);
    
    -    const hasMessage = await browser.hasText("sample message");
    -    expect(hasMessage).toEqual(true);
    -
         const messagesElement = await browser.find({css: ".messages-wrapper"});
         const messagesText = await messagesElement.evaluate(node => node.innerText);
         expect(messagesText).toContain("sample message");

--- .qawolf/tests/deleteMessage.test.js
+++ .qawolf/tests/deleteMessage.test.js
@@ -15,15 +15,15 @@ describe('deleteMessage', () => {
     await browser.type(selectors[1], "message to be deleted");
     await browser.click(selectors[2]);
 
-    const hasMessage = await browser.hasText("message to be deleted");
-    expect(hasMessage).toEqual(true);
-
-    const messagesElement = await browser.find({css: ".messages-wrapper"});
-    const messagesText = await messagesElement.evaluate(node => node.innerText);
+    let messagesElement = await browser.find({css: ".messages-wrapper"});
+    let messagesText = await messagesElement.evaluate(node => node.innerText);
     expect(messagesText).toContain("message to be deleted");
 
     // delete message
     await browser.click(selectors[3]);
-    expect(hasMessage).toEqual(false);
+
+    messagesElement = await browser.find({css: ".messages-wrapper"});
+    messagesText = await messagesElement.evaluate(node => node.innerText);
+    expect(messagesText).not.toContain("message to be deleted");
   });
 });
