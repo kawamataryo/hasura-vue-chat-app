c0f164b9845bfce6fba4dec4665558d2bbe88a77
ryo <ba068082@gmail.com>
2020-01-17 20:56:08 +0900

    delete confirm

--- .qawolf/tests/sendMessage.test.js
+++ .qawolf/tests/sendMessage.test.js
@@ -14,6 +14,9 @@ describe('sendMessage', () => {
     await browser.type(selectors[1], "sample message");
     await browser.click(selectors[3]);
 
+    const hasMessage = await browser.hasText("sample message");
+    expect(hasMessage).toEqual(true);
+
     const messagesElement = await browser.find({css: ".messages-wrapper"});
     const messagesText = await messagesElement.evaluate(node => node.innerText);
     expect(messagesText).toContain("sample message");
